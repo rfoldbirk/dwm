a1df1cd2af11bd96afc887d2810556a96836ca42
Rasmus Foldberg <rasmusfoldberg@gmail.com>
2021-02-17 03:16:38 +0100

    Basic setup

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono-regular:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "JetBrainsMono:size=10";
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#2e3440";
 static const char col_gray3[]       = "#d8dee9";
@@ -22,7 +22,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4" };
+static const char *tags[] = { "main", "view", "code", "work", "music" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -41,9 +41,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[ ]",      tile },    /* first entry is default */
+	{ "",         tile },    /* first entry is default */
 	{ "< >",      NULL },    /* no layout function means floating behavior */
-	{ "[m]",      monocle },
+	{ "[ ]",      monocle },
 };
 
 /* key definitions */
